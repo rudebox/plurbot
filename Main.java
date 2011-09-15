af5fe7e5216b8c4b6341dd6cc0fed5a0a65144df
Jason Few <jkcfew@googlemail.com>
2011-09-15 04:28:21 +0100

    update

--- Main.java
+++ Main.java
@@ -4,15 +4,22 @@ public class Main
 {
 	public static void main(String[] args) throws Exception 
 	{
-		String channel = args[1];
-		//startup
-		WasteBot wb = new WasteBot(args[0], channel);
-		//enable debug
-		wb.setVerbose(true);
-		//connect
-		wb.connect("irc.aberwiki.org");
-		//join	
-		wb.joinChannel(channel);
+		if(args.length == 2)
+		{
+			String channel = args[1];
+			//startup
+			WasteBot wb = new WasteBot(args[0], channel);
+			//enable debug
+			wb.setVerbose(true);
+			//connect
+			wb.connect("irc.aberwiki.org");
+			//join	
+			wb.joinChannel(channel);
+		}
+		else
+		{
+			System.out.println("Invalid Syntax: Please add arguments for name and channel.");
+			System.out.println("For example: java -classpath pircbot.jar:. Main \"Paul\" \"#mychannel\"");
+		}
 	}
-		
 }
